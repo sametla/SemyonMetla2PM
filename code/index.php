53d66ab3ff5c7310a55407fd404ebc6a98d643b2
sametla <smetla@stud.kantiana.ru>
2022-03-06 16:19:19 +0200

    1b Regular Expressions

--- code/index.php
+++ code/index.php
@@ -7,3 +7,9 @@ $regex = '/a..b/';
 $matches = [];
 preg_match_all($regex, $str, $matches);
 print_r($matches);
+
+echo "<br><br>1b. Regular Expressions<br><br>";
+
+$str = 'a1b2c3';
+$regex = '/(\d)+/';
+echo preg_replace_callback($regex, fn($matches) => intval(pow($matches[0],3)), $str);
