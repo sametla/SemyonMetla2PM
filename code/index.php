f8d0e8421972869d8f934e76e9a30e6456367bed
sametla <smetla@stud.kantiana.ru>
2022-03-06 15:28:16 +0200

    1a regular expressions

--- code/index.php
+++ code/index.php
@@ -1,1 +1,9 @@
 <?php
+
+echo "1a. Regular Expressions <br><br>";
+
+$str='ahb acb aeb aeeb adcb axeb';
+$regex = '/a..b/';
+$matches = [];
+preg_match_all($regex, $str, $matches);
+print_r($matches);
